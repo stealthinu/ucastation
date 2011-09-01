2cf8ee2d8ea24259b5ff99faebd355efee8c0160
SATOH Kiyoshi <satoh@hakuba.jp>
2011-09-01 18:40:58 +0900

    管理者からのお知らせ機能実装
    管理者からboard送られたら改行変換してクライアントへ送る
    現在のところ中身チェックしてないので悪意の管理者からのXSS攻撃がありうる
    view.htmlはindex.htmlになっていて古くなってるので削除

--- ucastation.js
+++ ucastation.js
@@ -25,34 +25,40 @@ io.set( "log level", 1 );
 
 var view_num = 0; // view用で接続してるクライアント数
 var channel; // デフォルトはチャンネル未選択
+var board = ""; // 管理者からのお知らせ
 
-// view用の接続
+// viewとの通信
 var view = io.of( '/view' ).on( 'connection', function( client ) {
   var address = client.handshake.address;
   console.log( "connect new client. " +
                "address:" + address.address + " port:" + address.port );
 
+  // 新しい接続があったら管理者へ現在の接続数を送信
   view_num ++;
   admin.emit( 'view num', view_num );
   
-  // 接続した時既にチャンネルが選択されていたらそれを開かせる
+  // 接続時、クライアントに現在のチャンネルと管理者からのお知らせを送信
   if ( channel ) {
     client.emit( 'channel', channel );
   }
+  view.emit( 'board', board );
 
+  // 切断処理
   client.on( 'disconnect', function() {
 	console.log( "disconnect" );
+    // 管理者へ減った接続数を送信
     view_num --;
     admin.emit( 'view num', view_num );
   });
 });
 
-// adminからの接続
+// adminとの通信
 var admin = io.of( '/admin' ).on( 'connection', function( client ) {
   var address = client.handshake.address;
   console.log( "connect new admin. " +
                "address:" + address.address + " port:" + address.port );
 
+  // 管理者のログインリクエスト
   client.on( 'admin login', function( password, fn ) {
     if ( password == conf.admin_password ) {
       console.log( "admin login success." );
@@ -65,6 +71,7 @@ var admin = io.of( '/admin' ).on( 'connection', function( client ) {
     }
   });
 
+  // チャンネル変更
   client.on( 'admin channel', function( ch, fn ) {
     console.log( "admin channel" );
     channel = ch;
@@ -72,6 +79,15 @@ var admin = io.of( '/admin' ).on( 'connection', function( client ) {
     fn( "OK admin channel" );
   });
 
+  // 管理者からのお知らせ変更
+  client.on( 'admin board', function( text, fn ) {
+    console.log( "admin board" );
+    board = text;
+    view.emit( 'board', board );
+    fn( "OK admin board" );
+  });
+
+  // 切断処理
   client.on( 'disconnect', function() {
 	console.log( "disconnect" );
   });
